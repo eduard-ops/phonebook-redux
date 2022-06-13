91b4f11bd2b838dad792274f3a61ac040fb48568
eduard-ops <epustynnik@gmail.com>
2022-06-13 09:46:21 +0300

    contacts-reducer.js

---
+++ src/redux/contacts/contacts-reducer.js
@@ -0,0 +1,39 @@
+import { combineReducers } from 'redux';
+
+import contactsTypes from './contacts-types';
+
+const contacts = (
+  state = JSON.parse(window.localStorage.getItem('contacts')) ?? [],
+  { type, payload }
+) => {
+  switch (type) {
+    case contactsTypes.ADD:
+      return [...state, payload];
+    case contactsTypes.SHOW:
+      return state.filter(({ name }) => name && name.includes(payload));
+    case contactsTypes.DELETE:
+      return state.filter(({ id }) => id !== payload);
+    default:
+      return state;
+  }
+};
+
+const filter = (state = '', { type, payload }) => {
+  switch (type) {
+    case contactsTypes.CHANGE_FILTER:
+      return payload;
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  contacts,
+  filter,
+});
+
+// const a = [{ name: 'jon', hobby: 'box' }];
+
+// const b = a.find(item => item.name === 'jox');
+
+// console.log(b);
