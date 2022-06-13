2b52b6368b38e97c4ee310043162d7794e523269
eduard-ops <epustynnik@gmail.com>
2022-06-13 09:46:26 +0300

    contacts-types.js

---
+++ src/redux/contacts/contacts-types.js
@@ -0,0 +1,22 @@
+const ADD = 'contacts/Add';
+
+const DELETE = 'contacts/Delete';
+
+const VERIFICATE = 'contacts/Verificate';
+
+const SHOW = 'contacts/Show';
+
+const CHANGE_FILTER = 'contacts/ChangeFilter';
+
+const FORM_SUBMIT = 'contacts/FormSubmit';
+
+const contactsTypes = {
+  ADD,
+  DELETE,
+  VERIFICATE,
+  SHOW,
+  CHANGE_FILTER,
+  FORM_SUBMIT,
+};
+
+export default contactsTypes;
