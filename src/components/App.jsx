1bf429de9e7b68d832563db2d81a9351e451eb7d
eduard-ops <epustynnik@gmail.com>
2022-06-14 11:14:50 +0300

    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,35 @@ import Container from './Container';
 
 import Filter from './Filter';
 
-export default function App() {
+import { connect } from 'react-redux';
+
+import { addContact } from 'redux/contacts/contacts-actions';
+
+function App({ onSubmit, contacts }) {
+  const checkName = name => {
+    const isValidate = contacts.find(item => item.text.name === name);
+    isValidate && alert(`${name} is already in contacts`);
+    return isValidate;
+  };
+
   return (
     <Container>
       <h1>Phonebook</h1>
-      <Form />
+      <Form onSubmit={onSubmit} checkName={checkName} />
       <h2>Contacts</h2>
       <Filter />
       <Contacts />
     </Container>
   );
 }
+
+const mapStateToProps = state => ({
+  contacts: state.contacts.contacts,
+  name: state.form,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onSubmit: text => dispatch(addContact(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
